135e6af19a811c7a47175f3a18602d9dfa7a7f76
Matouš Skála <skala.matous@gmail.com>
2013-01-07 17:54:49 +0100

    AsyncImageHelper

---
+++ src/com/skala/jellydroid/file/FileDownloader.java
@@ -0,0 +1,103 @@
+package com.skala.jellydroid.file;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import android.os.AsyncTask;
+import android.os.Environment;
+
+/**
+ * FileDownloader helps with downloading and storing remote files.
+ * @author Matous Skala
+ *
+ */
+public class FileDownloader {
+	private final static String CACHE_DIR = "Android/.jellydroid";
+
+	/**
+	 * Downloads a file from URL and saves in external storage or loads from cache if already downloaded.
+	 * @param url URL of the file.
+	 * @param listener Listener to be notified about status.
+	 */
+	public void download(String url, FileDownloadListener listener) {
+		File cachedFile = getCachedFile(url);
+		if (cachedFile.exists()) {
+			listener.onSuccess(cachedFile);
+		} else {
+			new FileDownloadTask(url, listener).execute();
+		}
+	}
+
+	private File getCachedFile(String url) {
+		return new File(getFileName(url));
+	}
+
+	private String getPath() {
+		return Environment.getExternalStorageDirectory() + "/" + CACHE_DIR;
+	}
+
+	private String getFileName(String url) {
+		return getPath() + "/" + url.hashCode();
+	}
+
+	private class FileDownloadTask extends AsyncTask<Object, Object, File> {
+		private final String mUrl;
+		private final FileDownloadListener mListener;
+
+		public FileDownloadTask(String url, FileDownloadListener listener) {
+			mUrl = url;
+			mListener = listener;
+		}
+
+		@Override
+		protected File doInBackground(Object... args) {
+			File file = null;
+			try {
+				URL url = new URL(mUrl);
+				URLConnection connection = url.openConnection();
+				connection.connect();
+
+				InputStream input = new BufferedInputStream(url.openStream());
+				File path = new File(getPath());
+				path.mkdirs();
+				String fileName = getFileName(mUrl);
+				OutputStream output = new FileOutputStream(fileName);
+
+				byte data[] = new byte[1024];
+				int count;
+				while ((count = input.read(data)) != -1) {
+					output.write(data, 0, count);
+				}
+
+				output.flush();
+				output.close();
+				input.close();
+
+				file = new File(fileName);
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return file;
+		}
+
+		@Override
+		protected void onPostExecute(File result) {
+			if (result != null) {
+				mListener.onSuccess(result);
+			}
+		}
+	}
+
+	public interface FileDownloadListener {
+		public void onSuccess(File file);
+	}
+}
---
+++ src/com/skala/jellydroid/image/AsyncImageHelper.java
@@ -0,0 +1,54 @@
+package com.skala.jellydroid.image;
+
+import java.io.File;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.widget.ImageView;
+
+import com.skala.jellydroid.file.FileDownloader;
+import com.skala.jellydroid.file.FileDownloader.FileDownloadListener;
+
+/**
+ * Helper that makes loading remote images quick and easy.
+ * @author Matous Skala
+ *
+ */
+public class AsyncImageHelper {
+	private final FileDownloader mDownloader = new FileDownloader();
+
+	public AsyncImageHelper() { }
+
+	/**
+	 * Loads image from URL into ImageView asynchronously.
+	 * @param image ImageView to which load the image.
+	 * @param url Remote image URL.
+	 */
+	public void loadImage(ImageView image, String url) {
+		loadImage(image, url, null);
+	}
+
+	/**
+	 * Loads image from URL into ImageView asynchronously.
+	 * @param image ImageView to which load the image.
+	 * @param url Remote image URL.
+	 * @param listener Listener to be notified when load is finished.
+	 */
+	public void loadImage(final ImageView image, String url, final AsyncImageListener listener) {
+		mDownloader.download(url, new FileDownloadListener() {
+			@Override
+			public void onSuccess(File file) {
+				Bitmap bm = BitmapFactory.decodeFile(file.getAbsolutePath());
+				image.setImageBitmap(bm);
+
+				if (listener != null) {
+					listener.onLoad(bm);
+				}
+			}
+		});
+	}
+
+	public interface AsyncImageListener {
+		public void onLoad(Bitmap bitmap);
+	}
+}
